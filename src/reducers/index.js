bbfd19b9732a619c689b61477c2d6be1d35323c7
Kamila Pantoja <kms.pantoja@gmail.com>
2022-09-27 18:17:27 -0400

    projeto trybewallet

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+// import user from './user';
+// import wallet from './wallet';
+// Kamila was here
+
+// Configure os seus reducers.
+// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
